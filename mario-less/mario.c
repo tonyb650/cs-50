f2152f67707969ba3fd85f1e9ddbf70b0868b6de
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2023-12-28 17:15:06 +0000

    Thu, Dec 28, 2023, 9:15 AM -08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,16 +4,21 @@
 int main(void)
 {
     int height = 0;
-    do {
+    do
+    {
         height = get_int("Height: ");
-    } while (height < 1 || height >8);
+    }
+    while (height < 1 || height >8);
     for(int i = 1; i <= height; i++)
     {
         for(int j = 1 ; j <= height ; j++)
         {
-            if(j<=height-i){
+            if(j<=height-i)
+            {
                 printf(" ");
-            } else {
+            }
+            else
+            {
                 printf("#");
             }
         }
