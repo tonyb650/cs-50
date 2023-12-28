861820949cc8eeb437b428f821e95904df26d04a
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2023-12-28 17:09:00 +0000

    Thu, Dec 28, 2023, 9:09 AM -08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
     {
         for(int j = 1 ; j <= height ; j++)
         {
-            if(j<height-i){
+            if(j<=height-i){
                 printf(" ");
             } else {
                 printf("#");
