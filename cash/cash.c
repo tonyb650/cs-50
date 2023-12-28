be733823975f81a19ba8e3072e4aab933b44d563
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2023-12-28 18:28:10 +0000

    Thu, Dec 28, 2023, 10:28 AM -08:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,8 +10,11 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents();
-
+    do {
+        int cents = get_cents();
+    }
+    while (cents > 1);
+    
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
     cents = cents - quarters * 25;
