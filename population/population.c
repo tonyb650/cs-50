447d90ba300cdaba57a517536347721adc96f5c0
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2023-12-28 02:22:25 +0000

    Wed, Dec 27, 2023, 6:22 PM -08:00

---
+++ population/population.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int n = 0;
+    while(n<=9)
+    {
+        n = get_int("Positive Integer: ");
+    }
+    printf(" %i\n",n);
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
