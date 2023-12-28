2752aa5c833b24e72a2d820fcb9c0e442df6b113
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2023-12-28 02:28:47 +0000

    Wed, Dec 27, 2023, 6:28 PM -08:00

--- population/population.c
+++ population/population.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -7,12 +8,22 @@ int main(void)
     int n = 0;
     while(n<=9)
     {
-        n = get_int("Positive Integer: ");
+        n = get_int("Enter starting llama population. Positive Integer: ");
     }
     printf(" %i\n",n);
     // TODO: Prompt for end size
-
+    int e = 0;
+    while(e<n)
+    {
+        e = get_int("Enter ending llama population. Positive Integer: ");
+    }
+    printf(" %i\n",e);
     // TODO: Calculate number of years until we reach threshold
-
+    int years = 0;
+    while(n<e){
+        n = n + trunc(n/3) - trunc(n/4);
+        years++;
+    }
     // TODO: Print number of years
+    printf("Years: %i\n", years);
 }
