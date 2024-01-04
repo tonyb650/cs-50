5c0fdc34759461fbe6b92df75c113a483da830f5
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-04 02:08:10 +0000

    Wed, Jan 3, 2024, 6:08 PM -08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,12 +7,13 @@ int main(int argc, string argv[])
 {
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext: ");
-    int n = strlen(plaintext);
-    string ciphertext[n];
-    for (int i = 0; i < n; i++)
-    {
-        char c = plaintext[i];
-        ciphertext = plaintext[i];
-    }
-    printf("ciphertext: %s\n", ciphertext);
+    printf("c: %c\n", plaintext[0]);
+    // int n = strlen(plaintext);
+    // char* ciphertext[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char c = plaintext[i];
+    //     ciphertext[i] = c;
+    // }
+    // printf("ciphertext: %s\n", ciphertext);
 }
