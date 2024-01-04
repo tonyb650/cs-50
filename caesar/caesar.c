68355b977424d90d3e841cfedfe50454f25ae795
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-04 02:12:54 +0000

    Wed, Jan 3, 2024, 6:12 PM -08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,9 +5,14 @@
 
 int main(int argc, string argv[])
 {
+    if(argc < 2) {
+        printf("Key is needed.\n");
+        return 1;
+    }
+    printf("argc= %i\n", argc);
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext: ");
-    printf("c: %c\n", plaintext[0]);
+    printf("c: %i\n", plaintext[0]);
     // int n = strlen(plaintext);
     // char* ciphertext[n];
     // for (int i = 0; i < n; i++)
