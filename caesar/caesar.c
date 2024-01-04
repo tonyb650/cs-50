4d957da49fcae63469370a812ba45f995aaae45b
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-04 01:29:58 +0000

    Wed, Jan 3, 2024, 5:29 PM -08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    int key = argv[1];
+    string plaintext = get_string("plaintext: ");
 }
