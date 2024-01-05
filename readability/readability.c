5a586ddc19bc632f29b7b8e545639b5993e55877
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-05 23:11:18 +0000

    Fri, Jan 5, 2024, 3:11 PM -08:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,7 +8,7 @@ int main(void)
 {
     string textBlock = get_string("Text: ");
     int characters = strlen(textBlock);
-    int scentenceEnds = 0;
+    int sentences = 0;
     int spaces = 0;
     int letters = characters;
     for (int i = 0; i < characters; i++)
@@ -20,35 +20,27 @@ int main(void)
         }
         else if (textBlock[i] == 46 || textBlock[i] == 33 || textBlock[i] == 63) // period, exclamation mark, question mark
         {
-            scentenceEnds++;
+            sentences++;
             letters--;
         }
-        else if (textBlock[i] <65 || textBlock[i] > 122 || (textBlock[i] > 90 && textBlock[i] < 97)) // other punctuation
+        else if (textBlock[i] <65 || textBlock[i] > 122 || (textBlock[i] > 90 && textBlock[i] < 97)) // other punctuation or numbers
         {
             letters--;
         }
     }
-    printf("Spaces %i\n", spaces);
-    printf("scentenceEnds %i\n", scentenceEnds);
-    printf("Letters %i\n", letters);
-    // where L is the average number of letters per 100 words in the text,
-    // and S is the average number of sentences per 100 words in the text.
-    // Steps:
-    // - get word count
+
     int words = spaces + 1;
-    // - get letter count
 
     // calculate L (letters/(words/100))
+    // where L is the average number of letters per 100 words in the text,
     float L = (float) letters / ((float) words/100);
-    // - get number of sentences
-    int sentences = scentenceEnds;
-    // - calculate S (sentences/(words/100))
-    float S = (float) sentences / ((float)words/100);
-    printf("S %f\n", S);
-    printf("L %f\n", L);
 
+    // calculate S (sentences/(words/100))
+    // where S is the average number of sentences per 100 words in the text.
+    float S = (float) sentences / ((float)words/100);
+
+    // Finally, calculate index
     float index = round(0.0588 * L - 0.296 * S -15.8);
-    // int index = 3;
     if (index < 1)
     {
         printf("Before Grade 1\n");
