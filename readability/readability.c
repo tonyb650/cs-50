f8711ab4b4655f227273e0669485290319dad514
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-05 22:43:56 +0000

    Fri, Jan 5, 2024, 2:43 PM -08:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,10 +6,49 @@
 int main(void)
 {
     string textBlock = get_string("Text: ");
-    int grade = 3;
+    int characters = strlen(textBlock);
+    int periods = 0;
+    int spaces = 0;
+    int letters = characters;
+    for (int i = 0; i < characters; i++)
+    {
+        if (textBlock[i] == 32) // space
+        {
+            spaces++;
+            letters--;
+        }
+        else if (textBlock[i] == 46) //period
+        {
+            periods++;
+            letters--;
+        }
+        else if (textBlock[i] <65 || textBlock[i] > 122 || (textBlock[i] > 90 && textBlock[i] < 97)) // other punctuation
+        {
+            letters--;
+        }
+    }
+    printf("Spaces %i\n", spaces);
+    printf("periods %i\n", periods);
+    printf("Letters %i\n", letters);
     // index = 0.0588 * L - 0.296 * S -15.8;
     // where L is the average number of letters per 100 words in the text,
     // and S is the average number of sentences per 100 words in the text.
-    printf("Grade %i\n", grade);
+    // Steps:
+    // - get word count
+    
+    // - get letter count
+    // calculate L (letters/(words/100))
+    // - get number of sentences
+    // - calculate S (sentences/(words/100))
+    //
+    int index = 3;
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
     return 0;
 }
