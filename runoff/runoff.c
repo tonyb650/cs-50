b059899f71832f56f8340a917b4e2adcd9953547
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-01-15 02:52:11 +0000

    Sun, Jan 14, 2024, 6:52 PM -08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,7 +133,7 @@ bool vote(int voter, int rank, string name)
         {
             if(strcmp(candidates[i].name, name) == 0)
             {
-                preferences[]
+                preferences[voter][i]++;
                 return true;
             }
         }
