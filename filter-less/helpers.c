24033059f584288514d8e525de5b9f8b08a09e87
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-11 23:15:41 +0000

    Sun, Feb 11, 2024, 3:15 PM -08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width, column++)
+        {
+            
+        }
+    }
     return;
 }
 
