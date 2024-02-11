05052bcf26a690e32766c8cde5d02250081aa598
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-11 21:48:02 +0000

    Sun, Feb 11, 2024, 1:48 PM -08:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,6 +34,12 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    BYTE b;
+
+    while (fread(&b, sizeof(b), 1, src) != 0)
+    {
+        fwrite(&b, sizeof(b), 1, dst);
+    }
 
     // TODO: Read samples from input file and write updated data to output file
 
