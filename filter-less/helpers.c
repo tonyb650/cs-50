a59a6319d6e1900670b5cc59ed8aaf7dc4a7da1d
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-12 04:59:09 +0000

    Sun, Feb 11, 2024, 8:59 PM -08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,6 +26,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE pixel = image[row][column];
             int pixel_brightness = (pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3;
+
             image[row][column].rgbtBlue = pixel_brightness * 0.5;
             image[row][column].rgbtGreen = pixel_brightness;
             image[row][column].rgbtRed = pixel_brightness;
@@ -36,6 +37,24 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int row = 0; row < height; row++)
+    {
+        RGBTRIPLE tempRow[width];
+        for (int column = 0; column < width; column++)
+        {
+            tempRow[column] = image[row][column];
+         }
+        for (int column = 0; column < width; column++)
+        {
+            image[row][column] = tempRow[width-column];
+         }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int row = 0; row < height; row++)
     {
@@ -78,24 +97,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[row][column-1].rgbtRed = (tempRow[column+1].rgbtRed+tempRow[column].rgbtRed+tempRow[column-1].rgbtRed)/3;
             // image[row][column].rgbtGreen = pixel_brightness;
             // image[row][column].rgbtRed = pixel_brightness;
-         }
-    }
-    return;
-}
-
-// Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int row = 0; row < height; row++)
-    {
-        RGBTRIPLE tempRow[width];
-        for (int column = 0; column < width; column++)
-        {
-            tempRow[column] = image[row][column];
-         }
-        for (int column = 0; column < width; column++)
-        {
-            image[row][column] = tempRow[width-column];
          }
     }
     return;
