3a63c7b60c0cdfa5b8b26db92593f43dccc99f58
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-12 16:54:55 +0000

    Mon, Feb 12, 2024, 8:54 AM -08:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,9 +5,43 @@ int main(int argc, char *argv[])
 {
      // Accept a single command-line argument
 
+    // Ensure proper usage
+    if (argc !=  2)
+    {
+        printf("Usage: ./recover infile\n");
+        return 3;
+    }
+
+    // Remember filename
+    char *infile = argv[1];
+    // printf("infile %s\n",infile);
+
     // Open the memory card
-
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 4;
+    }
     // While there's still data left to read from the memory card
-
+    while (fread(&sample, sample_size, 1, input) != 0)
+    {
+        sample *= factor;
+        fwrite(&sample, sample_size, 1, output);
+        counter++;
+    }
         // Create JPEGs from the data
+    // Open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 5;
+    }
+
+
+
+
+
 }
