6ce70bf614edbda780c840995e6490cc9cf134c0
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-12 04:08:00 +0000

    Sun, Feb 11, 2024, 8:08 PM -08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -27,9 +27,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         {
             RGBTRIPLE pixel = image[row][column];
             int pixel_brightness = (pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3;
-            image[row][column].rgbtBlue = pixel_brightness;
-            image[row][column].rgbtGreen = pixel_brightness*;
-            image[row][column].rgbtRed = pixel_brightness;
+            image[row][column].rgbtBlue = pixel_brightness*.9;
+            image[row][column].rgbtGreen = pixel_brightness*1.1;
+            image[row][column].rgbtRed = pixel_brightness*1.1;
          }
     }
     return;
