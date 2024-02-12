345fb582883838d02b949d93f2382e2041c2c5ce
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-12 04:16:32 +0000

    Sun, Feb 11, 2024, 8:16 PM -08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,7 +20,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    {
     for (int row = 0; row < height; row++)
     {
         for (int column = 0; column < width; column++)
@@ -35,12 +34,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-    return;
-}
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            RGBTRIPLE pixel = image[row][column];
+            int pixel_brightness = (pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3;
+
+            image[row][column].rgbtBlue = pixel_brightness * 0.5;
+            image[row][column].rgbtGreen = pixel_brightness;
+            image[row][column].rgbtRed = pixel_brightness;
+         }
+    }
     return;
 }
 
