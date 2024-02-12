197a2133b5b9c4a1e8242b338f97f578841fd838
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-02-12 04:22:44 +0000

    Sun, Feb 11, 2024, 8:22 PM -08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -43,20 +43,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         RGBTRIPLE tempRow[width];
         for (int column = 0; column < width; column++)
         {
-            // RGBTRIPLE pixel = image[row][column];
-            // int pixel_brightness = (pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3;
-
-            // image[row][column].rgbtBlue = pixel_brightness * 0.5;
-            // image[row][column].rgbtGreen = pixel_brightness;
             tempRow[column] = image[row][column];
          }
         for (int column = 0; column < width; column++)
         {
-            // RGBTRIPLE pixel = image[row][column];
-            // int pixel_brightness = (pixel.rgbtBlue + pixel.rgbtGreen + pixel.rgbtRed)/3;
-
-            // image[row][column].rgbtBlue = pixel_brightness * 0.5;
-            // image[row][column].rgbtGreen = pixel_brightness;
             image[row][column] = tempRow[width-column];
          }
     }
@@ -66,5 +56,17 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int row = 0; row < height; row++)
+    {
+        RGBTRIPLE tempRow[width];
+        for (int column = 0; column < width; column++)
+        {
+            tempRow[column] = image[row][column];
+         }
+        for (int column = 0; column < width; column++)
+        {
+            image[row][column] = tempRow[width-column];
+         }
+    }
     return;
 }
