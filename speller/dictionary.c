0f60edce417b5ab4d06c9d1f1b6acc25ddc3e71e
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-04-23 19:54:38 +0000

    Tue, Apr 23, 2024, 12:54 PM -07:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,12 +36,12 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    FILE *dictFile = fopen(dictionary, "r");
-    if (dictFile == NULL)
-    {
-        printf("Could not open %s.\n", dictionary);
-        return 1;
-    }
+    // FILE *dictFile = fopen(dictionary, "r");
+    // if (dictFile == NULL)
+    // {
+    //     printf("Could not open %s.\n", dictionary);
+    //     return 1;
+    // }
     return false;
 }
 
