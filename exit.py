2a4cdb9aeafd9e2c9221a328bb9d5fa4e7fc7550
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-12 03:15:31 +0000

    Tue, Jun 11, 2024, 8:15 PM -07:00

---
+++ exit.py
@@ -0,0 +1,7 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Missing command line argument")
+    sys.exit()
+
+print(f"Hello, {sys.argv[1]}")
--- greet.py
+++ greet.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-if len(argv) == 2:
+if len(argv) > 1:
     print(f"hello, {argv[1]}")
 else:
     print("Hello, world\n")
