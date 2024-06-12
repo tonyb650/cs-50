f00fe956cb0d3d62738271a5162c4511e66fb002
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-12 02:53:08 +0000

    Tue, Jun 11, 2024, 7:53 PM -07:00

--- scores.py
+++ scores.py
@@ -1,5 +1,12 @@
-scores = [72,73,33]
+from cs50 import get_int
 
+scores = []
+
+for i in range(3):
+    score = get_int("Score: ")
+    scores = scores + [score]
+    # scores.append(score)
+
+print(scores)
 average = sum(scores) / len(scores)
-
 print(f"average= {average}")
