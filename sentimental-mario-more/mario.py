21d3e02db85c8b5a72b1011aa31303fde631cb91
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-13 23:48:01 +0000

    Thu, Jun 13, 2024, 4:48 PM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,7 +5,6 @@ while height < 1 or height > 8:
     height = get_int("Height: ")
 for level in range(height):
     spaces = ""
-    for i in range(level-1):
+    for i in range(height-level-1):
         spaces = spaces + " "
     print(spaces + "#"*(level+1) + "  " + "#"*(level+1))
-# print (f"Hello, {answer}")
