09fd7cafc320c32b97ef7477964ad076ab0b7799
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-27 21:53:31 +0000

    Thu, Jun 27, 2024, 2:53 PM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,11 +11,11 @@ for char in text:
         sentences = sentences + 1
     if((ord(char) >= 65 and ord(char) <=90) or (ord(char) >= 97 and ord(char) <=122)):
         letters = letters + 1
-print(f"letters= {letters}")
-print(f"words= {words}")
-print(f"sentences= {sentences}")
-print(f"letters per 100 words = {letters/words*100}")
-print(f"sentences per 100 words = {sentences/words*100}")
+# print(f"letters= {letters}")
+# print(f"words= {words}")
+# print(f"sentences= {sentences}")
+# print(f"letters per 100 words = {letters/words*100}")
+# print(f"sentences per 100 words = {sentences/words*100}")
 
 grade = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
