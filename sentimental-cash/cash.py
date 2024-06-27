a5d8188b3f1eb1ef2ce6bb1e1813bfd1113e88e2
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-27 21:17:32 +0000

    Thu, Jun 27, 2024, 2:17 PM -07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,5 +5,14 @@ while (change < 0):
     change = get_float("Change: ")
 
 coins = 0
-# while ()
-print (f"Hello, {change}")
+while (change > 0):
+    if(change >= .25):
+        change = change - .25
+    elif (change >= .10):
+        change = change - .10
+    elif (change >= .05):
+        change = change - .05
+    else:
+        change = change - .01
+    coins = coins + 1
+print (coins)
