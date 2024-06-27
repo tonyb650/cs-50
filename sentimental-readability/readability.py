3172dbeadb33944449d41f4878321d76e6a4e0c7
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-27 21:42:58 +0000

    Thu, Jun 27, 2024, 2:42 PM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -12,6 +12,6 @@ for char in text:
     if((ord(char) >= 65 and ord(char) <=90) or (ord(char) >= 97 and ord(char) <=122)):
         letters = letters + 1
 
-grade = round(0.0588 * (letters/words/100) - 0.296 * (sentences/words/100) - 15.8)
+grade = round(0.0588 * (letters / words / 100) - 0.296 * (sentences / words / 100) - 15.8)
 
 print(f"Grade, {grade}")
