38bad43e40ab5fe4c283c1a1e61639ce34d3c7fe
Anthony Brierly <133928291+tonyb650@users.noreply.github.com>
2024-06-27 21:42:11 +0000

    Thu, Jun 27, 2024, 2:42 PM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,14 +2,14 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 letters = 0
-words = 0
+words = 1
 sentences = 0
 for char in text:
     if(char == " "):
         words = words + 1
     if(char == "." or char == "!" or char == "?"):
         sentences = sentences + 1
-    if((ord(char) >= 65 and ord(char) <=90) or (ord(char) >= 97 and ord(char) <=122))
+    if((ord(char) >= 65 and ord(char) <=90) or (ord(char) >= 97 and ord(char) <=122)):
         letters = letters + 1
 
 grade = round(0.0588 * (letters/words/100) - 0.296 * (sentences/words/100) - 15.8)
